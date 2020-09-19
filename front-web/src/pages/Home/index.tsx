a9240f920f7335b8eb680a3a451dfc4364ff5329
paulorcastro <paulo.rcastro@me.com>
2020-09-19 16:44:40 -0300

    Criando página de listagem dos registros

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import { homedir } from 'os';
+
 import React from 'react';
 import {ReactComponent as ArrowIcon} from '../../assets/arrow.svg';
 import {ReactComponent as GamerImage} from '../../assets/gamer.svg';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
@@ -17,14 +17,14 @@ const Home = () => (
                 Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
             </h3>
             <Link to="/records">
-                <div className="home-actions">
-                    <button className="home-btn">
-                        QUERO SABER QUAIS SÃO
-                    </button>
-                    <div className="home-btn-icon">
-                        <ArrowIcon />
-                    </div>
+            <div className="home-actions">
+                <button className="home-btn">
+                    QUERO SABER QUAIS SÃO
+                </button>
+                <div className="home-btn-icon">
+                    <ArrowIcon />
                 </div>
+            </div>
             </Link>
         </div>
         <GamerImage className="home-image" />
